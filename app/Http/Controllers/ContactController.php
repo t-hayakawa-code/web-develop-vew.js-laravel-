045e1504fd660ac8969c677dd0abb34255f24a8f
t.hayakawa <hayakawa.sou.lol@gmail.com>
2021-03-19 00:54:09 +0900

    checkCode

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\contacts;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $items = contacts::all();　//Contactがcontactsに変えてあります
+        return response()->json([
+            'message' => 'OK',
+            'data' => $items
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $item = new contacts;
+        $item->name = $request->name;
+        $item->email = $request->email;
+        $item->save();
+        return response()->json([
+            'message' => 'Created successfully',
+            'data' => $item
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\contacts  $contacts
+     * @return \Illuminate\Http\Response
+     */
+    public function show(contacts $contacts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\contacts  $contacts
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, contacts $contacts)
+    {
+        $item = contacts::where('id', $contacts->id)->first();
+        $item->name = $request->name;
+        $item->email = $request->email;
+        $item->save();
+        if ($item) {
+            return response()->json([
+                'message' => $item,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => 'Not found',
+            ], 404);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\contacts  $contacts
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(contacts $contacts)
+    {
+        $item = contacts::where('id', $contacts->id)->delete();
+        if ($item) {
+            return response()->json([
+                'message' => $item,
+            ], 200);
+        } else {
+            return response()->json([
+                'message' => $item,
+            ], 404);
+        }
+    }
+}
